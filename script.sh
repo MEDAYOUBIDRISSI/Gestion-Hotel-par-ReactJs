75c1a67083f431cfd4959f4015b26e855f0094cd
MedAyoubIdrissi <medayoub.idrissi@gmail.com>
2020-04-17 22:33:48 +0100

    first commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/MAI202020/Gestion-Hotel-par-ReactJs.git
+git push -f origin master
+
+
+#git clone https://github.com/MAI202020/Gestion-Hotel-par-ReactJs.git
+#git pull origin master
