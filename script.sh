7297814b34acd154db9c396cddbef63624081880
MedAyoubIdrissi <medayoub.idrissi@gmail.com>
2020-05-01 17:56:57 +0000

    Add asset folders +1

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 git init
 git add .
-git commit -m "Add asset folders"
+git commit -m "Add asset folders +1"
 git remote add origin https://github.com/MAI202020/Gestion-Hotel-par-ReactJs.git
 git push -f origin master
 
